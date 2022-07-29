16b2fbd4ecb897a029c1ad5fffb9e1b532f9c567
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2022-07-29 18:39:57 -0400

    chore: passed user and token in response

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,15 +9,21 @@ use App\Models\User;
 class LoginController extends Controller
 {
     public function login(Request $request)
-    {   
+    {
         $user = User::firstWhere('email', $request->email);
 
         if (!$user) {
-            return response()->errorResponse('no user found, please register.', [], 401);
+            return response()->errorResponse('invalid credentials', [], 401);
         }
 
         if (!auth()->attempt(['email' => $request->email, 'password' => $request->password])){
-            return response()->errorResponse('password is incorrect.', [], 401);
+            return response()->errorResponse('invalid credentials', [], 401);
         }
+
+        $token = auth()->user()->createToken('job board');
+
+        $data = array_merge(['token' => $token->accessToken], auth()->user()->toArray());
+
+        return response()->success('login successful', $data);
     }
 }
