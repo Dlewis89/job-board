e6278e169586975a8b4fb4a95dab99cfc1762198
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2022-08-02 17:38:33 -0400

    chore: add loginrequest form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         $user = User::firstWhere('email', $request->email);
 
