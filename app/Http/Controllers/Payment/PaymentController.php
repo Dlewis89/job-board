fa154ea0c058b2d800a0f9e39f971d7b4b020ab5
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2022-09-27 14:27:41 -0400

    feat: finish webhook and stripe signature implementation

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -7,31 +7,52 @@ use Illuminate\Http\Request;
 use App\Exceptions\CustomException;
 use App\Models\TransactionLog;
 use App\Models\Job;
+use App\Http\Requests\Payment\StripeFormRequest;
 
 class PaymentController extends Controller
 {
-    public function webhook(Request $request)
+    public function webhook(StripeFormRequest $request)
     {
-        $type = $request['type'];
-        $data = $request['data']['object'];
-        switch($type) {
-            case 'payment_intent.succeeded':
-                return $this->handlePaymentSuccessful($data);
-            case 'payment_intent.failed':
-                return $this->handlePaymentFailed($data);
-            default:
-                throw new CustomException('Something went wrong');
+        try {
+            $type = $request['type'];
+            $data = $request['data']['object'];
+            switch($type) {
+                case 'payment_intent.succeeded':
+                    return $this->handlePaymentSuccessful($data);
+                case 'payment_intent.failed':
+                    return $this->handlePaymentFailed($data);
+                default:
+                    throw new CustomException('Something went wrong');
+            }
+        } catch(CustomException $e) {
+            return response()->errorResponse($e->getMessage(), [], $e->getCode());
+        } catch(\Exception $e) {
+            return response()->errorResponse('Something went wrong!');
         }
     }
 
     public function handlePaymentSuccessful($data)
     {
         $transaction =  TransactionLog::where('reference', $data['metadata']['reference'])->firstOrFail();
-        // if($transaction->status === 'success'){
-        //     throw new CustomException('Duplicate webhook request', 400);
-        // }
+        if($transaction->status === 'success'){
+            throw new CustomException('Duplicate webhook request', 400);
+        }
         $transaction->update(['status' => 'success']);
         Job::find($data['metadata']['job_id'])->update(['is_featured' => true]);
+        return response()->success('Success');
+    }
+
+    public function handlePaymentFailed($data)
+    {
+        $transaction =  TransactionLog::where('reference', $data['metadata']['reference'])->firstOrFail();
+        if($transaction->status === 'success'){
+            throw new CustomException('Status already updated.', 400);
+        }
+        if($transaction->status === 'failed'){
+            throw new CustomException('Duplicate webhook request', 400);
+        }
+        $transaction->update(['status' => 'failed']);
+
         return response()->success('Success');
     }
 }
---
+++ app/Http/Requests/Payment/StripeFormRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\Payment;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Carbon\Carbon;
+use App\Exceptions\CustomException;
+
+class StripeFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->hasHeader('Stripe-Signature')) {
+            $sig = $this->header('Stripe-Signature');
+            info($sig);
+            $data = explode(",", $sig);
+            $time_split = explode("=", $data[0]);
+            $v1_split = explode("=", $data[1]);
+            $v0_split = ($data[2] ?? false) ? explode("=", $data[2]) : null;
+            $timestamp = ($time_split[1]);
+            $signed_payload = $timestamp. '.' . json_encode($this->all());
+            $expected_sig = hash_hmac('sha256', config('app.stripe_secret_key'), $signed_payload);
+            if(Carbon::createFromTimestamp(time())->diffInDays(now()) >= config('app.stripe_tolerance')){
+                throw new CustomException("Webhook tolerance exceeded", 400);
+            };
+            return ($expected_sig === $v1_split[1] || $expected_sig === $v0_split ? $v0_split[1] : false);
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
